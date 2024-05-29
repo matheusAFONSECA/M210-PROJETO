07ad6e6dbf79b280322f1048c03fbf0af2b47f71
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-05-29 14:16:52 -0300

    Venv

---
+++ main2.py
@@ -0,0 +1,9 @@
+# Importe as bibliotecas necessárias
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Escreva diretamente no aplicativo
+st.title("Exemplo de Aplicativo Streamlit 🎈")
+st.write("""
+    Substitua este exemplo pelo seu próprio código!
+""")
