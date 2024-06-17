f5850da2417902f5e1a0a735050fac3356c495f1
Matheus Pereira <49734641+mathzpereira@users.noreply.github.com>
2024-06-17 15:38:05 -0300

    data editor funcionando

--- functions.py
+++ functions.py
@@ -1,38 +1,27 @@
 from time import sleep
 import streamlit as st
 import pandas as pd
+from scipy.optimize import linprog
 
 # Função que cria a tabela simplex dada a quantidade de variáveis e restrições
 def create_simplex_table(num_vars, num_constraints):
     st.subheader("Tabela Simplex")
     
     # Criação de uma tabela vazia
-    simplex_table = pd.DataFrame(columns=[f"x{i+1}" for i in range(num_vars)] + ["LD"],
+    simplex_table = pd.DataFrame(columns=[f"x{i+1}" for i in range(num_vars)] + [f"s{i+1}" for i in range (num_constraints)] + ["LD"],
                                  index=[f"Restrição {j+1}" for j in range(num_constraints)] + ["Função Objetivo"])
     
     # Preenchimento da tabela com zeros (ou valores iniciais)
     simplex_table[:] = 0
 
-    # Create a dynamic data editor
-    edit_df = st.data_editor(simplex_table, height=400)
+    return simplex_table
 
-# Check if there are changes in the editor
-    if edit_df is not None:
-    # Get the updated 'x' column from the editor
-        x_values = edit_df['x']
-
-    # Calculate the corresponding 'y' values
-        y_values = x_values ** 2
-
-    # Update the 'y' column in the original DataFrame
-        simplex_table['y'] = y_values
-
-    if st.button("Resolver Simplex"):
-        st.write("Simplex Resolvido")
-        st.write(simplex_table)
-        st.write("Solução: ")
-        st.write("x1 = 1, x2 = 2, x3 = 3, LD = 4")
-        st.write("Valor da Função Objetivo: 10")
-        st.write("Solução Ótima Encontrada!")
-        sleep(5)
-   
+# Função que resolve o tableau simplex
+def solve_tableau(simplex_table):
+    optimized_tableau = linprog(c=simplex_table.loc["Função Objetivo", :].values[:-1],
+                                A_ub=simplex_table.loc[:"Restrição 1", :].values[:, :-1],
+                                b_ub=simplex_table.loc[:"LD", :].values[:, -1],
+                                method="simplex")
+    
+    st.write("Solução do problema:")
+    st.write(optimized_tableau)
--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from functions import *
+from scipy.optimize import linprog
 
 cols = 2
 rows = 2
@@ -11,11 +12,24 @@ def main():
     # Entrada do número de variáveis (colunas) e restrições (linhas)
     num_vars = st.number_input("Número de Variáveis (Colunas)", min_value=1, max_value=4, step=1)
     num_constraints = st.number_input("Número de Restrições (Linhas)", min_value=1, max_value=4, step=1)
+    table_created = False
 
     # Criação da tabela Simplex com base nas entradas do usuário
     if st.button("Criar Tabela Simplex"):
-        create_simplex_table(num_vars, num_constraints)
 
+        st.session_state['initial_simplex'] = create_simplex_table(num_vars, num_constraints)
+    
+    # Verifica se a tabela foi criada e exibe a data editor
+    if 'initial_simplex' in st.session_state:
+        st.write("Edite a tabela abaixo:")
+        st.session_state['updated_simplex'] = st.data_editor(st.session_state['initial_simplex'])
+
+        table_created = True
+
+    # Solução do problema de programação linear
+    if table_created:
+        if st.button("Resolver Tableau"):
+            solve_tableau(st.session_state['updated_simplex'])
 
 # Executar a aplicação Streamlit
 if __name__ == '__main__':
